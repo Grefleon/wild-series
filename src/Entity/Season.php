4c2ee45644897bdcf6efa6e5f47dcf445b111095
Oleg Chenesseau <oleg.chenesseau@gmail.com>
2020-06-08 18:38:25 +0200

    Correction label

--- src/Entity/Season.php
+++ src/Entity/Season.php
@@ -133,4 +133,9 @@ class Season
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->getProgram()->getTitle() . ' - Saison ' . $this->getNumber();
+    }
 }
